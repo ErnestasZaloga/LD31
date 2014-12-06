597dcca4394b4380ebdb363a0671096879c54a72
Vyciokz <vytautas.sedelskis@gmail.com>
2014-12-06 12:20:50 +0200

    monstro stuff

--- LD31/src/com/us/ld31/game/Foe.java
+++ LD31/src/com/us/ld31/game/Foe.java
@@ -1,24 +1,47 @@
 package com.us.ld31.game;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.IntArray;
+import com.us.ld31.utils.Astar;
 import com.us.ld31.utils.SpriteActor;
 
 public class Foe extends SpriteActor{
 	
-	private float movementSpeed;
+	private float movementSpeed = 10;
+	private boolean shouldTravel;
 	
-	public Foe(TextureRegion region) {
+	
+	private final GameWorld world;
+	private final Astar astar;
+	
+	private IntArray path;
+	private int pathIndex = 0;
+	
+	public Foe(TextureRegion region, final GameWorld world) {
 		super(region);
-		
-		
+		this.world = world;
+		this.astar = world.getAstar();
 	}
 	
 	@Override
 	public void act(float delta) {
 		super.act(delta);
 		
-		//setPosition(Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
+		if(shouldTravel) {
+			if(pathIndex >= path.size) {
+				shouldTravel = false;
+				pathIndex = 0;
+			} else {
+				this.setPosition(path.get(pathIndex++), path.get(pathIndex++));
+			}
+		}
+	}
+	
+	public void travelTo(float x, float y) {
+		//path = astar.getPath((int)getX(), (int)getY(), (int)x, (int)y);
+		path = astar.getPath((int)x, (int)y, (int)getX(), (int)getY());
+		shouldTravel = true;
+		pathIndex = 0;
 	}
 	
 }
--- LD31/src/com/us/ld31/game/GameWorld.java
+++ LD31/src/com/us/ld31/game/GameWorld.java
@@ -1,10 +1,13 @@
 package com.us.ld31.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.us.ld31.LD31;
 import com.us.ld31.utils.Astar;
 import com.us.ld31.utils.TouchListener;
@@ -14,6 +17,7 @@ public class GameWorld extends Group {
 	private final LD31 app;
 	private final Character character;
 	private final Astar astar;
+	private Foe foe;
 	
 	public GameWorld(final LD31 app) {
 		this.app = app;
@@ -30,15 +34,40 @@ public class GameWorld extends Group {
 		pixmap.setColor(Color.GREEN);
 		pixmap.fillCircle(5, 5, 5);
 		
-		astar = new Astar(0, 0, null);
+		astar = new Astar(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), new Astar.Listener() {
+			
+			@Override
+			public boolean isValid(int x, int y) {
+				// TODO Auto-generated method stub
+				return true;
+			}
+		});
 		
-		Foe foe = new Foe(new TextureRegion(new Texture(pixmap)));
+		addListener(new TouchListener() {
+			@Override
+			public boolean touchDown(final InputEvent event, 
+								  	 final float x, 
+								  	 final float y, 
+								  	 final int pointer, 
+								  	 final int button) {
+				foe.travelTo(x, y);
+				
+				
+				
+				return true;
+			}
+		});
 		
+		foe = new Foe(new TextureRegion(new Texture(pixmap)), this);
 		addActor(foe);
 	}
 	
 	public void begin() {
 		addActor(character);
+	}
+	
+	public Astar getAstar() {
+		return astar;
 	}
 	
 }

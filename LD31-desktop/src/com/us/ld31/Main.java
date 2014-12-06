85303322c66786f90f8f03cecc8f39d6210b7619
pauliusj <jurgelevicius.paulius@gmail.com>
2014-12-06 10:49:28 +0200

    WIndow resize

--- LD31-desktop/src/com/us/ld31/Main.java
+++ LD31-desktop/src/com/us/ld31/Main.java
@@ -7,8 +7,8 @@ public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "LD31";
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.width = 1024;
+		cfg.height = 600;
 		
 		new LwjglApplication(new LD31(), cfg);
 	}

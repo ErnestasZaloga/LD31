a0b491f9909a8c4d0d37e12dfc9439e01b8800bf
Vyciokz <vytautas.sedelskis@gmail.com>
2014-12-06 11:21:49 +0200

    monstras :/

---
+++ LD31/src/com/us/ld31/game/Foe.java
@@ -0,0 +1,12 @@
+package com.us.ld31.game;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.us.ld31.utils.PooledSpriteActor;
+
+public class Foe extends PooledSpriteActor{
+	
+	public Foe(TextureRegion region) {
+		setRegion(region);
+	}
+	
+}
--- LD31/src/com/us/ld31/game/GameWorld.java
+++ LD31/src/com/us/ld31/game/GameWorld.java
@@ -1,5 +1,9 @@
 package com.us.ld31.game;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.us.ld31.LD31;
 
@@ -9,6 +13,14 @@ public class GameWorld extends Group {
 	
 	public GameWorld(final LD31 app) {
 		this.app = app;
+		
+		Pixmap pixmap = new Pixmap(10, 10, Pixmap.Format.RGBA8888);
+		pixmap.setColor(Color.GREEN);
+		pixmap.fillCircle(5, 5, 5);
+		
+		Foe foe = new Foe(new TextureRegion(new Texture(pixmap)));
+		
+		addActor(foe);
 	}
 	
 	public void begin() {

412b76132ffe9cd8baccb1b547ef992b364d8563
Ernyz <ernestas.zaloga@gmail.com>
2014-12-07 15:08:36 +0200

    More translocation spells added.

--- LD31/src/com/us/ld31/game/GameWorld.java
+++ LD31/src/com/us/ld31/game/GameWorld.java
@@ -12,10 +12,7 @@ import com.us.ld31.game.PlayerCharacter.SkillSlot;
 import com.us.ld31.game.foestuff.Foe;
 import com.us.ld31.game.foestuff.FoeManager;
 import com.us.ld31.game.skills.DebugSkillTree;
-<<<<<<< HEAD
-import com.us.ld31.game.skills.translocations.BlinkOthersAway;
-=======
->>>>>>> e8da438ca30f88130b9f4060b200304df11af62b
+import com.us.ld31.game.skills.translocations.BlinkOthersCloser;
 import com.us.ld31.game.ui.Delegate;
 import com.us.ld31.game.ui.GameUi;
 import com.us.ld31.game.ui.SkillBar.SkillButton;
@@ -138,7 +135,9 @@ public class GameWorld extends Group {
 //			BlinkOther skill = new BlinkOther();
 //			BlinkAwayOther skill = new BlinkAwayOther();
 //			Portal skill = new Portal();
-			BlinkOthersAway skill = new BlinkOthersAway();
+//			BlinkOthersAway skill = new BlinkOthersAway();
+//			BlinkOtherCloser skill = new BlinkOtherCloser();
+			BlinkOthersCloser skill = new BlinkOthersCloser();
 			skill.activate(character, this, 1);
 		}
 			
